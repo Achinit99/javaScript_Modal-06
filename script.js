a2572a7054b9e0d7eefd34fb3d4f24b10d2f5776
Achinit99 <achinit720@gmail.com>
2022-10-14 13:46:14 +0530

    Project #2_Modal Window

--- script.js
+++ script.js
@@ -1,1 +1,7 @@
-"use strict";
+'use strict';
+
+const modal = document.querySelector('.modal');
+const overlay = document.querySelector('.overlay');
+const btnCloseModal = document.querySelector('.close-modal');
+const btnOpenModal = document.querySelectorAll('.show-modal');
+console.log(btnOpenModal);
